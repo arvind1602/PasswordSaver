9a0089241f3882e73e4883b463adc54d39f71db2
arvind1602 <arvindnagpure72230@gmail.com>
2025-07-31 00:33:26 +0530

    change cors url

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "https://passwordsaver-bice.vercel.app",
     credentials: true,
   })
 );
